8f04fd2330d1db7b9d4dc7caa832ff03cd3ac46c
Luo-Ying <luoyingqifr@gmail.com>
2025-11-28 12:39:29 +0100

    feat: introduce project-detail crawler
    - Added a new Crawler service to handle robots.txt, retry & caching logic, and fetch project details
    - Extended Makefile and docker-compose, optimizing Ollama startup and health checks

--- app/main.py
+++ app/main.py
@@ -32,27 +32,27 @@ def get_codeur_new_project_matched():
     project_list = []
 
     # filter emails by email_from and email_title and is_matched_project
-    for email_id in unread_emails:
-        email = mail_box.getEmail(email_id)
-        email_obj = Email(email)
-        email_title, email_from, email_content = email_obj.parse_email()
-        if "notification@compte.codeur.com" not in email_from or "Nouveau projet" not in email_title:
-            continue
-        if not is_matched_project(email_content):
-            continue
-        # TODO: get matched project details and convert to project object
+    # for email_id in unread_emails:
+    #     email = mail_box.getEmail(email_id)
+    #     email_obj = Email(email)
+    #     email_title, email_from, email_content = email_obj.parse_email()
+    #     if "notification@compte.codeur.com" not in email_from or "Nouveau projet" not in email_title:
+    #         continue
+    #     if not is_matched_project(email_content):
+    #         continue
+    #     # TODO: get matched project details and convert to project object
 
 
         # project = convert_emailcontent_to_project(email_content)
         # project_list.append(project)
     
-    # email = mail_box.getEmail(unread_emails[500])
-    # email_obj = Email(email)
-    # email_title, email_from, email_content = email_obj.parse_email()
-    # # print("email content: ", email_content, "\n")
-    # print("email from: ", email_from, "\n")
-    # print("email title: ", email_title, "\n")
-    # print("is matched project: ", is_matched_project(email_content), "\n")
+    email = mail_box.getEmail(unread_emails[500])
+    email_obj = Email(email)
+    email_title, email_from, email_content = email_obj.parse_email()
+    # print("email content: ", email_content, "\n")
+    print("email from: ", email_from, "\n")
+    print("email title: ", email_title, "\n")
+    print("is matched project: ", is_matched_project(email_content), "\n")
 
 
     # print("project list: ", project_list, "\n")
